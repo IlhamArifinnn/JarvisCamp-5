b409b040d21333eabd342c5e940c222a80da4671
ilham arifin <arifinn2208@gmail.com>
2024-05-26 09:04:02 +0700

    Commit: pertemuan7

---
+++ w-4/7/jarvis-5/resources/views/task/index.blade.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Daftar Tugas</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <div class="container">
+        <h1 class="text-center">Daftar Tugas</h1>
+        <hr>
+
+        <div class="row">
+            @foreach ($tasks as $task)
+                <div class="col-12 col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title">{{ $task['name'] }}</h4>
+                            <small>Deadline: {{ \Carbon\Carbon::parse($task['deadline'])->format('d F Y') }}</small><br>
+                            <span class="badge bg-warning">{{ $task['status'] }}</span>
+                            <p class="card-text">{{ Str::limit($task['description'], 40, '...') }}</p>
+
+                            <!-- Updated snippet of resources/views/tasks/index.blade.php -->
+                            <div class="mt-2">
+                                <a href="{{ route('task.detail', $task['id']) }}" class="btn btn-primary">Detail</a>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
