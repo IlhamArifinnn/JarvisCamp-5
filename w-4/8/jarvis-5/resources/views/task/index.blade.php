e8c764e4e689225e92ff15aa46301e786086209d
ilham arifin <arifinn2208@gmail.com>
2024-05-30 07:47:40 +0700

    commit: add sbadmin to MANTU

--- w-4/8/jarvis-5/resources/views/task/index.blade.php
+++ w-4/8/jarvis-5/resources/views/task/index.blade.php
@@ -1,24 +1,13 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Daftar Tugas</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-
-<body>
-
+@extends('template')
+@section('content')
     <div class="container">
-        <h1 class="text-center">Daftar Tugas</h1>
+        <h2 class="text-center mt-3">Daftar Tugas</h2>
         <hr>
 
         <div class="row">
             @foreach ($tasks as $task)
                 <div class="col-12 col-md-4">
-                    <div class="card">
+                    <div class="card mb-3">
                         <div class="card-body">
                             <h4 class="card-title">{{ $task['name'] }}</h4>
                             <small>Deadline: {{ \Carbon\Carbon::parse($task['deadline'])->format('d F Y') }}</small><br>
@@ -36,10 +25,11 @@
             @endforeach
         </div>
     </div>
+@endsection
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+</script>
 </body>
 
 </html>
--- w-4/8/jarvis-5/routes/web.php
+++ w-4/8/jarvis-5/routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('template');
+    return view('dashboard.index');
 });
 
 Route::get('/tasks', [TaskController::class, 'index']);
